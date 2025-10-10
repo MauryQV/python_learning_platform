7bd2e71948950b8ae7beb7ce64fb07cfa2221c53
Valeria <valeriadaza54@gmail.com>
2025-10-09 21:48:46 -0400

    Tarea:PATCH /admin/users/:id/role

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -2,7 +2,6 @@ import express from "express";
 import { updateUserRole } from "../controllers/admin.controller.js";
 import { verifyToken, isAdmin } from "../middleware/auth/auth.middleware.js";
 
-
 const router = express.Router();
 
 // PATCH /admin/users/:id/role

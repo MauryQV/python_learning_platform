05731868f29d1e09ae4f0707bc25ace7ffd9b379
VICENTE MAURICIO QUINSAMOLLE VARBAS <qmaury30@gmail.com>
2025-10-22 08:34:28 -0400

    solucionado el bug de verificar el token

--- backend/src/auth/tokenService.js
+++ backend/src/auth/tokenService.js
@@ -2,26 +2,30 @@ import jwt from "jsonwebtoken";
 
 const SECRET = process.env.JWT_SECRET || "super_secret_key";
 
-export const generateToken = async (user) => {
-  return jwt.sign(
-    {
-      userId: user.userId,
-      email: user.email,
-      role:
-        user.roles && user.roles.length > 0
-          ? user.roles[0].role.name
-          : user.role || "user",
-    },
-    SECRET,
-    { expiresIn: "1h" }
-  );
-};
-
-export const verifyToken = async (token) => {
-  try {
-    return jwt.verify(token, SECRET);
-  } catch (err) {
-    if (err.name === "TokenExpiredError") throw new Error("expired Token");
-    throw new Error("invalid Token");
+class TokenService {
+  generateToken(user) {
+    return jwt.sign(
+      {
+        userId: user.userId,
+        email: user.email,
+        role:
+          user.roles && user.roles.length > 0
+            ? user.roles[0].role.name
+            : user.role || "user",
+      },
+      SECRET,
+      { expiresIn: "1h" }
+    );
   }
-};
+
+  verifyToken(token) {
+    try {
+      return jwt.verify(token, SECRET);
+    } catch (err) {
+      if (err.name === "TokenExpiredError") throw new Error("expired Token");
+      throw new Error("invalid Token");
+    }
+  }
+}
+
+export default new TokenService();
